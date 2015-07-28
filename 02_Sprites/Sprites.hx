c0f1d9f2d5a52f51b37819ac024c57b8d0f217d9
YellowAfterlife <yellowafterlife@ymail.com>
2015-07-28 22:26:58 +0300

    Added 02_Sprites

---
+++ 02_Sprites/Sprites.hx
@@ -0,0 +1,23 @@
+package;
+import Pico.*;
+class Sprites {
+	static inline var num = 5;
+	static inline function main() {
+		// clear the screen:
+		cls();
+		var t:Fixed = 0;
+		onUpdate = function() {
+			t += 0.1;
+		};
+		onDraw = function() {
+			cls();
+			// draw some lizards:
+			for (i in 0 ... num) {
+				// (centering):
+				var x = i * 8 + (128 - num * 8) / 2;
+				var y = (128 - 8) / 2;
+				spr(flr(t + i) % 2, x, y);
+			}
+		}
+	}
+}
